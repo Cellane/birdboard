f543e928bacf29bd7518ba837aa7de91a768ea1b
Milan Vit <milanvit@milanvit.net>
2019-08-25 21:36:27 +0900

    Episode 31 Complete

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -107,6 +107,15 @@ class ManageProjectsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
+    {
+        $project = tap(ProjectFactory::create())->invite($this->signIn());
+
+        $this->get('/projects')
+            ->assertSee($project->title);
+    }
+
+    /** @test */
     public function an_authenticated_user_cannot_view_the_projects_of_others()
     {
         $this->signIn();
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -6,6 +6,8 @@ use Tests\TestCase;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 
 class UserTest extends TestCase
 {
@@ -14,8 +16,28 @@ class UserTest extends TestCase
     /** @test */
     public function a_user_has_projects()
     {
-        $user = factory('App\User')->create();
+        $user = factory(User::class)->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+        $nick = factory(User::class)->create();
+        $project = tap(ProjectFactory::ownedBy($sally)->create())->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
