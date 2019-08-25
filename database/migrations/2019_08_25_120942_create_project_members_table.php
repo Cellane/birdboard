72ad90acad6d9fd824b72f565a4032212544493b
Milan Vit <milanvit@milanvit.net>
2019-08-25 21:16:22 +0900

    Episode 30 Complete

---
+++ database/migrations/2019_08_25_120942_create_project_members_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->index(['project_id', 'user_id']);
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_members');
+    }
+}
---
+++ tests/Feature/InvitationsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
+
+class InvitationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_project_can_invite_a_user() {
+        $project = ProjectFactory::create();
+
+        $project->invite($newUser = factory(User::class)->create());
+
+        $this->signIn($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = [
+            'body' => 'Foo task'
+        ]);
+
+        $this->assertDatabaseHas('tasks', $task);
+    }
+}
