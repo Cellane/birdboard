3614e6d83e2b2e4dc20b530d9e4fe672d2bb6dd1
Milan Vit <milanvit@milanvit.net>
2019-08-25 22:57:11 +0900

    Episode 33 Complete

---
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ProjectInvitationRequest;
+use App\Project;
+use App\User;
+
+class ProjectInvitationsController extends Controller
+{
+    public function store(Project $project, ProjectInvitationRequest $request)
+    {
+        $user = User::whereEmail(request('email'))->first();
+
+        $project->invite($user);
+
+        return redirect($project->path());
+    }
+}
